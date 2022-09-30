0b085c278020ad61304c8d6a78547f69732eb6fa
julia.arruda <julia.arruda@julia.arruda>
2022-09-29 22:33:37 -0300

    client methods

--- cliente_servidor/cliente.c
+++ cliente_servidor/cliente.c
@@ -17,61 +17,33 @@
 #define KNRM  "\x1B[0m"
 #define EXIT_KEY_WORD  "EXIT"
 
-/** @brief Reads a port number from input. 
- *
- *  Related to item 4.
- *
- *  @return Port to be used on socket address.
- */
-int getPort() {
-    int port;
-
-    printf("Enter the port number: ");
-    scanf("%d", &port);
-
-    return port;
-}
-
-/** @brief Gets address from a given socket using getsockname.
- *
- *  Related to item 5.
- *
- *  @param connfd socket identifier.
- *  @param addrSize size of the returned address.
- *  @return socket address.
- */
-struct sockaddr_in getSockName(int sockfd, int addrSize) {
-    struct sockaddr_in addr;
-    socklen_t len = addrSize;
-    if (getsockname(sockfd, (struct sockaddr *) &addr, &len) < 0) {
+void GetSockName(int sockfd, struct sockaddr *addr, socklen_t * addrlen) {
+    if (getsockname(sockfd, addr, addrlen) < 0) {
         perror("getsockname");
         exit(1);
     }
-    return addr;
 }
 
-/** @brief Sends a message of maximum size MAXDATASIZE to a given open socket connection.
- *
- *  Related to item 7.
- *
- *  @param sockfd socket identifier.
- */
-void sendMessage(int sockfd) {
-    char message[MAXDATASIZE];
-    printf("Enter a message to send to the server: ");
-    scanf("%s", message);
-    write(sockfd, message, strlen(message));
+FILE* Popen(char* command, char* mode) {
+    FILE *fp;
+    if ((fp = popen(command, mode)) == NULL) {
+        perror("fputs error");
+        exit(1);
+    }
+    return fp;
+}
+
+void Fputs(const char *str, FILE *stream) {
+    if (fputs(str, stream) == EOF) {
+        perror("fputs error");
+        exit(1);
+    }
 }
 
 void sendCommandOutput(char* command, int sockfd){
-    FILE *fp;
+    FILE *fp = Popen(command, "r");
     char output[MAXDATASIZE] = {0};
     
-    if ((fp = popen(command, "r")) == NULL) {
-        perror("fputs error");
-        exit(1);
-    }    
-
     while(fgets(output, MAXDATASIZE, fp) != NULL) {
         // printf("DEBUG: %s", output);
         write(sockfd, output, MAXDATASIZE);
@@ -80,11 +52,8 @@ void sendCommandOutput(char* command, int sockfd){
     write(sockfd, eof, MAXDATASIZE);
 }
 
-int main(int argc, char **argv) {
-    int    sockfd, n;
-    char   recvline[MAXLINE + 1];
+void assertValidArgs(int argc, char **argv) {
     char   error[MAXLINE + 1];
-    struct sockaddr_in servaddr;
 
     if (argc != 3) {
         strcpy(error,"uso: ");
@@ -93,46 +62,72 @@ int main(int argc, char **argv) {
         perror(error);
         exit(1);
     }
+}
 
-    if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+int Socket(int family, int type, int flags) {
+    int sockfd;
+
+    if ((sockfd = socket(family, type, flags)) < 0) {
         perror("socket error");
         exit(1);
     }
+    
+    return sockfd;
+}
+
+void InetPton(int family, char* ip, void * port) {
+    if (inet_pton(family, ip, port) <= 0) {
+        perror("inet_pton error");
+        exit(1);
+    }
+}
+
+void Connect(int sockfd, struct sockaddr *servaddr, int size) {
+    if (connect(sockfd, (struct sockaddr *) &servaddr, size) < 0) {
+        perror("connect error");
+        exit(1);
+    }
+}
+
+int Read(int sockfd, void *buf, size_t count) {
+    int n = read(sockfd, buf, count);
+
+    if (n < 0) {
+        perror("read error");
+        exit(1);
+    }
+
+    return n;
+}
+
+int main(int argc, char **argv) {
+    int    sockfd, n;
+    char   recvline[MAXLINE + 1];
+    struct sockaddr_in servaddr;
+
+    assertValidArgs(argc, argv);
+    sockfd = Socket(AF_INET, SOCK_STREAM, 0);
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port   = htons(strtod(argv[2], NULL));
-    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0) {
-        perror("inet_pton error");
-        exit(1);
-    }
-
-    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
-        perror("connect error");
-        exit(1);
-    }
+    
+    InetPton(AF_INET, argv[1], &servaddr.sin_addr);
+    Connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
     
     time_t clock = time(NULL);
     printf("%s%.24s - Starting \n%s", KGRN, ctime(&clock), KNRM);
-    struct sockaddr_in addr = getSockName(sockfd, sizeof(servaddr));
+
+    struct sockaddr_in addr;
+    socklen_t len = sizeof(servaddr);
+    GetSockName(sockfd, (struct sockaddr *) &addr, &len);
     printf("Local IP address: %s\n", inet_ntoa(addr.sin_addr));
     printf("Local port      : %d\n", ntohs(addr.sin_port));
-
-    // sendMessage(sockfd);
     
-    while (((n = read(sockfd, recvline, MAXLINE)) > 0) && strcmp(recvline, EXIT_KEY_WORD))  {
+    while (((n = Read(sockfd, recvline, MAXLINE)) > 0) && strcmp(recvline, EXIT_KEY_WORD)) {
         recvline[n] = 0;
-        if (fputs(recvline, stdout) == EOF) {
-            perror("fputs error");
-            exit(1);
-        }
-        
+        Fputs(recvline, stdout);
         sendCommandOutput(recvline, sockfd);
-    }
-    
-    if (n < 0) {
-        perror("read error");
-        exit(1);
     }
 
     exit(0);
